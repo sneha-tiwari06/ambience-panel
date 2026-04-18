0354fcd021b23e8ccec69a579a630cc632912805
sneha-tiwari06 <114509946+sneha-tiwari06@users.noreply.github.com>
2026-04-18 11:52:29 +0530

    api updated

--- src/utils/axiosInstnace.js
+++ src/utils/axiosInstnace.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_URL = "https://backend-ambience.onrender.com/api";
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://backend-ambience.onrender.com/api";
+// const API_URL = "http://localhost:5000/api";
 
 export const IMAGE_URL = "https://images.ambience.in/";
 // export const IMAGE_URL = "http://localhost:5000/";
